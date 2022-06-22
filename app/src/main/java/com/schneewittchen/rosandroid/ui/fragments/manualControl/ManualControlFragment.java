62ba0aeba4e2f4b618eb1858f36697360ab0662c
ryanzhang15 <ryanjzhang@gmail.com>
2022-06-22 15:59:25 +0800

    broken code. edward wants it.

--- app/src/main/java/com/schneewittchen/rosandroid/ui/fragments/manualControl/ManualControlFragment.java
+++ app/src/main/java/com/schneewittchen/rosandroid/ui/fragments/manualControl/ManualControlFragment.java
@@ -1,7 +1,9 @@
 package com.schneewittchen.rosandroid.ui.fragments.manualControl;
 
+import android.app.Application;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,21 +14,33 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.schneewittchen.rosandroid.R;
 import com.schneewittchen.rosandroid.domain.RosDomain;
+import com.schneewittchen.rosandroid.model.repositories.rosRepo.node.BaseData;
 import com.schneewittchen.rosandroid.ui.fragments.map.MapxusFragment;
 
 import org.jetbrains.annotations.NotNull;
 
+import com.schneewittchen.rosandroid.ui.general.DataListener;
 import com.schneewittchen.rosandroid.ui.general.TabButton;
+import com.schneewittchen.rosandroid.viewmodel.VizViewModel;
+import com.schneewittchen.rosandroid.widgets.joystick.JoystickData;
 import com.schneewittchen.rosandroid.widgets.joystick.JoystickView;
+import com.schneewittchen.rosandroid.domain.RosDomain;
 
 import java.util.Objects;
 
 import sensor_msgs.Joy;
 
 public class ManualControlFragment extends Fragment {
+
+    private final static String TAG = ManualControlFragment.class.getSimpleName();
+    private final RosDomain rosDomain;
+
+    public ManualControlFragment() {
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
     @Nullable
@@ -38,24 +52,20 @@ public class ManualControlFragment extends Fragment {
         TabButton homeButton = new TabButton(v.findViewById(R.id.home_button_mc));
         homeButton.linkToFragment(0, getParentFragmentManager().beginTransaction());
 
-        //fucking disgusting code
-//        RosDomain instance = RosDomain.getInstance(this.requireActivity().getApplication());
-        //widget creation system
-//        instance.createWidget(v.getUniqueDrawingId(),"Joystick");
-        /*
-        For future reference, widget type is one of:
-        - Joystick
-        - Battery
-        - Button
-        - Camera
-        - Debug
-        - Label
-        - Logger
-        - Viz2D
-        - RqtPlot
-         */
 
+        //mad as shit rn
+        JoystickView joystick = v.findViewById(R.id.joystick);
 
+        joystick.setDataListener(new DataListener() {
+            @Override
+            public void onNewWidgetData(BaseData data) {
+//                Log.d(TAG, "onNewWidgetData: fuck this"+);
+//                Log.d(TAG, "onNewWidgetData: fuck");
+//                Log.d(TAG, "onNewWidgetData: fuck this "+joystick.getX()+", "+joystick.getY());
+//                Log.d(TAG, "onNewWidgetData: bitch "+data.toRosMessage());
+            }
+        }); // I fucking code!! I fucking code!!
+        //ryan's lost his fucking mind with this bitch code
 
         return v;
 
