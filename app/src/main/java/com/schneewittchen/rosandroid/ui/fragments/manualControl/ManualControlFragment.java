17595db2a642672fb383e6d3d654819f090d2a6e
ryanzhang15 <ryanjzhang@gmail.com>
2022-06-22 14:00:32 +0800

    Added a joystick on the manual control page.
    However, the joystick doesnt fucking work yet.

--- app/src/main/java/com/schneewittchen/rosandroid/ui/fragments/manualControl/ManualControlFragment.java
+++ app/src/main/java/com/schneewittchen/rosandroid/ui/fragments/manualControl/ManualControlFragment.java
@@ -1,5 +1,6 @@
 package com.schneewittchen.rosandroid.ui.fragments.manualControl;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,10 +9,12 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.schneewittchen.rosandroid.R;
+import com.schneewittchen.rosandroid.domain.RosDomain;
 import com.schneewittchen.rosandroid.ui.fragments.map.MapxusFragment;
 
 import org.jetbrains.annotations.NotNull;
@@ -19,8 +22,13 @@ import org.jetbrains.annotations.NotNull;
 import com.schneewittchen.rosandroid.ui.general.TabButton;
 import com.schneewittchen.rosandroid.widgets.joystick.JoystickView;
 
+import java.util.Objects;
+
+import sensor_msgs.Joy;
+
 public class ManualControlFragment extends Fragment {
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
@@ -29,6 +37,25 @@ public class ManualControlFragment extends Fragment {
 
         TabButton homeButton = new TabButton(v.findViewById(R.id.home_button_mc));
         homeButton.linkToFragment(0, getParentFragmentManager().beginTransaction());
+
+        //fucking disgusting code
+//        RosDomain instance = RosDomain.getInstance(this.requireActivity().getApplication());
+        //widget creation system
+//        instance.createWidget(v.getUniqueDrawingId(),"Joystick");
+        /*
+        For future reference, widget type is one of:
+        - Joystick
+        - Battery
+        - Button
+        - Camera
+        - Debug
+        - Label
+        - Logger
+        - Viz2D
+        - RqtPlot
+         */
+
+
 
         return v;
 
